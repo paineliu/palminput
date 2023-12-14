b2ba10f2d6060b69b5c9b3bba9250f528c7b40a5
liutingchao <liutingchao@hotmail.com>
2023-12-14 14:02:16 +0000

    add fcitx engine bui;d

--- project/linux/fcitx-palminput/do.sh
+++ project/linux/fcitx-palminput/do.sh
@@ -1,7 +1,16 @@
+cd ../../../engine
 rm -rf build
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel ..
+make
+mkdir ../lib/linux
+cp libpalmengine.a ../lib/linux/
+cd ../../project/linux/fcitx-palminput
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel ..
 make
 #sudo cp build/src/fcitx-googlepinyin.so /usr/lib/x86_64-linux-gnu/fcitx/fcitx-googlepinyin.so
 
